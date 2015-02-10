56160ccb620f90fd2f1e231b9650e88ee294190c
es3a10 <es3a10@gmail.com>
2015-02-10 07:17:43 +0100

    miscript

---
+++ miscript.sh
@@ -0,0 +1,4 @@
+git config --global user.name es3a10
+git config --global user.email es3a10@gmail.com
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
